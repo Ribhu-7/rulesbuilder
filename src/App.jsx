c451a831fa1d21699159fe854fe7bc4014aeb115
Ribhu7 <ribhughosh7@gmail.com>
2024-01-08 19:06:41 +0530

    message

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Parent from './components/Parent'
+
+function App() {
+
+  return (
+    <div className="container mt-5">
+      <Parent/>
+        
+    </div>
+  )
+}
+
+export default App
